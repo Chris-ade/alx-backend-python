04550efed7fa8c9e3cc3e5288fc8223f51318721
Chris Ade <101924502+Chris-ade@users.noreply.github.com>
2023-02-02 17:15:31 +0100

    Create 5-sum_list.py

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+5-sum_list: Module
+"""
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Takes a list input_list of floats as an arg and returns their sum as a float.
+    """
+    return sum(input_list)
