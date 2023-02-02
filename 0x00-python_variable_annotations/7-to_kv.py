ef25dd5a590faae48df5d1fc7cc346d91e7ba0e1
Chris Ade <101924502+Chris-ade@users.noreply.github.com>
2023-02-02 18:15:42 +0100

    Update 7-to_kv.py

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,6 +4,7 @@ String and int/float to tuple
 """
 from typing import Tuple, Union
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     Takes a string k and an int OR float v as arguments and returns a tuple.
