2c8f22494c7e5621f43274e6f0e50c927bd828f4
Chris Adebiyi <joeade12054@gmail.com>
2023-02-03 12:25:55 -0800

    Task done

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,13 +2,9 @@
 """
 Sum list : a module
 """
+from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
-    """ sum a list of float """
-    sum: float = 0
-    i = 0
-    for i in range(len(input_list)):
-        sum += input_list[i]
-
-    return sum
+    """ Sum of a list """
+    return sum(input_list)
