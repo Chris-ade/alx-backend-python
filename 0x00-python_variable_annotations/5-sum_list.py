7c3cb2d4e793e8de65dd79e01828fc263b8d4a1d
Chris Adebiyi <joeade12054@gmail.com>
2023-02-03 12:17:22 -0800

    Task done

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 """
-sum list : a module
+Sum list : a module
 """
 
 
-def sum_list(input_list: list[float]) -> float:
-    """
-    sum a list of float
-    """
+def sum_list(input_list: List[float]) -> float:
+    """ sum a list of float """
     sum: float = 0
     i = 0
     for i in range(len(input_list)):
