dc9625d43f8efaa1557ad8aa172a03f23cf94589
Chris Adebiyi <joeade12054@gmail.com>
2023-02-07 04:12:40 -0800

    Task done

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,14 +4,15 @@ The basics of async
 """
 import asyncio
 import random
+from typing import Optional, Union
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: Optional[Union[int, float]] = 10) -> float:
     """Wait for a random delay between 0 and max_delay.
 
     Args:
-        max_delay (int, optional): The maximum delay in seconds.
-            Defaults to 10.
+        max_delay (Optional[Union[int, float]], optional):
+        The max delay in secs. Defaults to 10.
 
     Returns:
         float: The delay in seconds.
