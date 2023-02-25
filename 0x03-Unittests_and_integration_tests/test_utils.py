2362331c7c40892ed527e91fcab27134c8e97f07
Chris Ade <101924502+Chris-ade@users.noreply.github.com>
2023-02-25 09:32:56 +0100

    Task done

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,6 +3,7 @@
 Parameterize a unit test
 """
 
+
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
@@ -18,14 +19,3 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected_output):
         """Test that access_nested_map returns the expected result."""
         self.assertEqual(access_nested_map(nested_map, path), expected_output)
-
-    @parameterized.expand([
-        ({}, ("a",)),
-        ({"a": 1}, ("a", "b"))
-    ])
-    def test_access_nested_map_exception(self, nested_map, path):
-        """Test that access_nested_map raises a KeyError for invalid paths."""
-        with self.assertRaises(KeyError) as context:
-            access_nested_map(nested_map, path)
-        expected_exception_msg = f"KeyError: '{path[-1]}'"
-        self.assertEqual(str(context.exception), expected_exception_msg)
